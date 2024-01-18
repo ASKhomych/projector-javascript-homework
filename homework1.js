feac370cac018c11e4b87103170057d6b587e36d
“Andrii <andriy.khomych@eze.tech>
2024-01-18 10:29:24 +0200

    the first homework

---
+++ homework1.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// a. додавання
+
+let sum1 = 23;
+let sum2 = 34;
+let total = sum1 + sum2;
+console.log(`All total is ${total}`)
+
+// b. віднімання
+
+let sellCars = 11;
+let totalCars = 200;
+let remainCars = totalCars - sellCars;
+console.log(`Cars remain: ${remainCars}`)
+
+// c. множення
+
+let cartApples = 3;
+let aplles = 12;
+let totalAppels = cartApples * aplles;
+console.log(`Numbers of apples: ${totalAppels}`)
+
+// d. ділення
+
+let students = 30;
+let books = 10;
+let result = students / books;
+console.log(`We have one book for three students: ${result}`)
+
+// e. зведення в ступінь
+
+let a = 5;
+let b = a ** 3;
+console.log(b)
