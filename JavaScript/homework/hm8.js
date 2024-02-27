a6db0c547d13166b6635ee827364e29ce946ba7b
“Andrii <andriy.khomych@eze.tech>
2024-02-27 09:24:21 +0200

    homework-8-fixed2

--- JavaScript/homework/hm8.js
+++ JavaScript/homework/hm8.js
@@ -5,22 +5,17 @@
 // Вона буде знаходити суму усіх своїх аргументів незалежно від їх кількості (але без використання вбудованого об'єкту Math).
 // Використайте оператор розширення
 
-console.log(addThemAll(2,4)); // 6
-console.log(addThemAll(1,2,3,4)); // 10
-console.log(addThemAll(5,5,10)); // 20
+// console.log(addThemAll(2,4)); // 6
+// console.log(addThemAll(1,2,3,4)); // 10
+// console.log(addThemAll(5,5,10)); // 20
 
-function addThemAll (...args) {
-    return args.reduce((sum, currentValue) => sum + currentValue, 0);
-}
-
+// function addThemAll // тут пишете свій код
 //   2. Задача на використання замикання. 
 //   Напишіть функцію яка працює таким чином: multiply(a)(b)  // a * b
-console.log(multiply(5)(5))		// 25
-console.log(multiply(2)(-2))	// -4
-console.log(multiply(4)(3))		// 12
+// console.log(multiply(5)(5))		// 25
+// console.log(multiply(2)(-2))	        // -4
+// console.log(multiply(4)(3))		// 12
 
-function multiply(a) {
-    return function(b) {
-        return a * b;
-    };
-}
+// function multiply(a) {
+// 	// тут ваш код*
+// }
