3c634a953298cdae792ca7c06b5c622f5e552b88
“Andrii <andriy.khomych@eze.tech>
2024-03-10 07:26:33 +0200

    homework-10-updated

--- JavaScript/homework/hm10.js
+++ JavaScript/homework/hm10.js
@@ -13,24 +13,24 @@
 function durationBetweenDates(startDateStr = '1970-01-01', endDateStr = '1970-01-01', unit = 'seconds') {
     const startDate = new Date(startDateStr);
     const endDate = new Date(endDateStr);
-    const diff = endDate - startDate;
+    let diff = Math.abs(endDate - startDate);
 
     switch (unit) {
         case 'days':
-            return `${Math.floor(diff / (1000 * 60 * 60 * 24))} days`;
+            return Math.floor(diff / (1000 * 60 * 60 * 24));
         case 'hours':
-            return `${Math.floor(diff / (1000 * 60 * 60))} hours`;
+            return Math.floor(diff / (1000 * 60 * 60));
         case 'minutes':
-            return `${Math.floor(diff / (1000 * 60))} minutes`;
+            return Math.floor(diff / (1000 * 60));
         case 'seconds':
-            return `${Math.floor(diff / 1000)} seconds`;
+            return Math.floor(diff / 1000);
         default:
             return 'Invalid unit';
     }
 }
 
-console.log(durationBetweenDates('2020-6-01', '2024-10-01', 'seconds')); 
-console.log(durationBetweenDates('2022-01-31', '2021-02-03', 'days')); 
+console.log(durationBetweenDates('2020-06-01', '2024-10-01', 'seconds')); 
+console.log(durationBetweenDates('2022-01-31', '2021-02-03', 'days'));
 
 
 // 2. Масив унікальних значень
